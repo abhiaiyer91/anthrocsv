7e3b90c0bdf704adb4ef9654b45d226d99c55453
abhiaiyer91 <abhiaiyer91@gmail.com>
2014-06-25 13:00:36 -0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name csvconverterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the csvconverterApp
+ */
+angular.module('csvconverterApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name csvconverterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the csvconverterApp
+ */
+angular.module('csvconverterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
